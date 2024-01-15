3690868f138e8aa7bcadaed7e5b3d845ea952605
Bishopaul <monitepaul@gmail.com>
2024-01-15 11:02:18 -0800

    Hello, World

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 three = word[:3]
 two = word[-2:]
-middle = word[1:8]
+middle = word[1:-1]
 print(f"First 3 letters: {three}")
 print(f"Last 2 letters: {two}")
 print(f"Middle word: {middle}")
