1d846c7aa7db68984ecd55fa02466937d4fbb63d
Bishopaul <monitepaul@gmail.com>
2024-01-17 20:55:53 -0800

    if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for number in range(100):
+        if number == 99:
+                    print(f"{number:d}")
+                        else:
+                                    print("{:02d}, ".format(number), end="")
